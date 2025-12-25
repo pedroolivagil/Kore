b8039d0372824d9f7d3265cfa9a77ec976bdd972
Pedro Oliva Gil <olivagp.ext@madrid.es>
2025-12-26 00:43:51 +0100

    v1.0.4

--- Kore/src/main/java/com/olivadevelop/kore/db/database/KoreDataBase.java
+++ Kore/src/main/java/com/olivadevelop/kore/db/database/KoreDataBase.java
@@ -1,9 +1,5 @@
 package com.olivadevelop.kore.db.database;
 
 import androidx.room.RoomDatabase;
-import androidx.room.TypeConverters;
 
-import com.olivadevelop.kore.db.converter.Converters;
-
-@TypeConverters({Converters.class})
 public abstract class KoreDataBase extends RoomDatabase { }
