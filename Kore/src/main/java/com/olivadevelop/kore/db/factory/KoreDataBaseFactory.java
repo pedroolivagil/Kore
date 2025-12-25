942c37a19e6e82e3560dfc6597ac71095ae64289
Pedro Oliva Gil <olivagp.ext@madrid.es>
2025-12-25 22:09:48 +0100

    v1.0.2

--- Kore/src/main/java/com/olivadevelop/kore/db/factory/KoreDataBaseFactory.java
+++ Kore/src/main/java/com/olivadevelop/kore/db/factory/KoreDataBaseFactory.java
@@ -8,5 +8,5 @@ import com.olivadevelop.kore.db.database.KoreDataBase;
 import java.util.function.Function;
 
 public abstract class KoreDataBaseFactory<DB extends KoreDataBase> extends DataBaseFactory<DB> {
-    KoreDataBaseFactory(DataBaseConfig<DB> config, Function<RoomDatabase.Builder<DB>, RoomDatabase.Builder<DB>> builder) { super(config, builder); }
+    public KoreDataBaseFactory(DataBaseConfig<DB> config, Function<RoomDatabase.Builder<DB>, RoomDatabase.Builder<DB>> builder) { super(config, builder); }
 }
