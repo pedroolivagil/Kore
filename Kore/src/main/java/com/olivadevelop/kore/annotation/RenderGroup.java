17dd974f75cabf7c183ebb5e6f22b059e77268c2
Pedro Oliva Gil <olivagp.ext@madrid.es>
2026-01-04 21:22:16 +0100

    v1.0.37

--- Kore/src/main/java/com/olivadevelop/kore/annotation/RenderGroup.java
+++ Kore/src/main/java/com/olivadevelop/kore/annotation/RenderGroup.java
@@ -7,9 +7,9 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface RenderInView {
+public @interface RenderGroup {
     /**
-     * @return ID View
+     * @return ID group
      */
     String value();
 }
