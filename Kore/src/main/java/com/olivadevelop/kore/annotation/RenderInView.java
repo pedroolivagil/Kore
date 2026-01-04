af3ff7ec4eeb0f4d3b5f9813796766e14fab47a3
Pedro Oliva Gil <olivagp.ext@madrid.es>
2026-01-04 21:19:15 +0100

    v1.0.37

--- Kore/src/main/java/com/olivadevelop/kore/annotation/RenderInView.java
+++ Kore/src/main/java/com/olivadevelop/kore/annotation/RenderInView.java
@@ -1,7 +1,5 @@
 package com.olivadevelop.kore.annotation;
 
-import com.olivadevelop.kore.activity.KoreActivity;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -10,5 +8,8 @@ import java.lang.annotation.Target;
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RenderInView {
-    Class<KoreActivity<?, ?>>[] value();
+    /**
+     * @return ID View
+     */
+    String value();
 }
