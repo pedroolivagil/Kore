808ec804721b730c3f7ce0cef9e1820a8e637c9a
Pedro Oliva Gil <olivagp.ext@madrid.es>
2026-02-03 22:22:59 +0100

    1.0.76

--- Kore/src/main/java/com/olivadevelop/kore/component/attribute/KoreAttributes.java
+++ Kore/src/main/java/com/olivadevelop/kore/component/attribute/KoreAttributes.java
@@ -1,22 +1,21 @@
 package com.olivadevelop.kore.component.attribute;
 
 import com.olivadevelop.kore.attributtes.KoreComponentViewAttribute;
+import com.olivadevelop.kore_annotations.GenerateXMLAttributes;
 
 import java.util.EnumMap;
 
+@GenerateXMLAttributes(styleable = "KoreComponentView")
 public class KoreAttributes {
-    private final EnumMap<KoreComponentViewAttribute, Object> values =
-            new EnumMap<>(KoreComponentViewAttribute.class);
+    private final EnumMap<KoreComponentViewAttribute, Object> values = new EnumMap<>(KoreComponentViewAttribute.class);
 
     public <T> void set(KoreComponentViewAttribute attr, T value) {
         values.put(attr, value);
     }
-
     @SuppressWarnings("unchecked")
     public <T> T get(KoreComponentViewAttribute attr) {
         return (T) values.getOrDefault(attr, attr.getType());
     }
-
     public boolean has(KoreComponentViewAttribute attr) {
         return values.containsKey(attr);
     }
--- Kore/src/main/java/com/olivadevelop/trigger/KoreComponentAttrsTrigger.java
+++
@@ -1,8 +0,0 @@
-package com.olivadevelop.trigger;
-
-import com.olivadevelop.kore_annotations.GenerateXMLAttributes;
-
-@GenerateXMLAttributes(styleable = "KoreComponentView")
-public final class KoreComponentAttrsTrigger {
-    private KoreComponentAttrsTrigger() { }
-}
