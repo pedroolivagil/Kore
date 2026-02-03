0135b388c2e123edf8baaa906fc9deb7bb5d4fa4
Pedro Oliva Gil <olivagp.ext@madrid.es>
2026-02-03 20:16:17 +0100

    1.0.74

--- Kore/src/main/java/com/olivadevelop/trigger/KoreComponentAttrsTrigger.java
+++ Kore/src/main/java/com/olivadevelop/trigger/KoreComponentAttrsTrigger.java
@@ -2,7 +2,7 @@ package com.olivadevelop.trigger;
 
 import com.olivadevelop.kore_annotations.GenerateXMLAttributes;
 
-//@GenerateXMLAttributes(styleable = "KoreComponentView")
+@GenerateXMLAttributes(styleable = "KoreComponentView")
 public final class KoreComponentAttrsTrigger {
     private KoreComponentAttrsTrigger() { }
 }
